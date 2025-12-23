3ef2909008526b5d03d915639641fced6f1ea7b7
Aniruddha Sil <aniruddha.sil@thoughtworks.com>
2025-12-23 16:08:46 +0530

    Update landing page UI and dependencies
    Enhanced the landing page with improved styling, updated section titles, and added navigation links to client-hydrated and streaming routes. Upgraded Next.js to 16.0.10 and React to 19.2.1. Renamed partial-pre-rendering route to streaming for clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,47 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-black w-full min-h-screen flex flex-col items-center text-white">
+    <main className="bg-black w-full min-h-screen flex flex-col items-center py-10 gap-20">
       <div className="py-10">
-        <h1 className="text-6xl font-raleway">Hydration Playground</h1>
+        <h1 className="text-9xl text-dusty-rose text-shadow-mauve-mist text-shadow-lg font-storyscript">
+          Hydration Playground
+        </h1>
       </div>
-      <div className="grid grid-cols-2 gap-10 w-3/4">
+      <div className="grid grid-cols-2 gap-10 w-3/4 pb-20">
         {[
           {
-            title: "Zone 1",
+            title: "Client Hydrated UI",
             description:
               "Fully client side hydrated. This component and all its children are rendered on the client.",
+            link: "/client-hydrated",
           },
           {
-            title: "Zone 2",
+            title: (
+              <>
+                Next.js Streaming with{" "}
+                <span className="w-min pb-1 bg-mauve-mist text-ivory-cream px-2">
+                  Suspense
+                </span>
+              </>
+            ),
             description:
               "Partial hydration (RSC). This is a server with a mix of server and client components.",
-          },
-          {
-            title: "Zone 3",
-            description:
-              "Progressive hydration - On Visibilty or Interaction. This component is rendered on the server and sent to the client as HTML, but it only hydrates when it becomes visible or the user interacts with it.",
-          },
-          {
-            title: "Zone 4",
-            description:
-              "Adaptive hydration - Based on Network/Device Capabilities. This component adapts its hydration strategy based on the user's network speed and device capabilities, providing an optimized experience.",
+            link: "/streaming",
           },
         ].map((zone, index) => (
-          <div className="border p-5 h-64" key={index}>
-            <h2 className="text-3xl font-raleway mb-5 uppercase">
+          <Link
+            className="border-4 border-dusty-rose bg-mauve-mist/10 p-8 rounded-2xl hover:bg-mauve-mist/20 transition-all duration-300 shadow-md shadow-dusty-rose/30 hover:shadow-lg hover:shadow-golden-amber/40"
+            key={index}
+            href={zone.link}
+          >
+            <h2 className="text-4xl font-storyscript mb-6 text-dusty-rose text-shadow-md text-shadow-mauve-mist">
               {zone.title}
             </h2>
-            <p className="font-nunito-sans tracking-wider">
+            <p className="font-nunito-sans tracking-wider text-ivory-cream leading-relaxed">
               {zone.description}
             </p>
-          </div>
+          </Link>
         ))}
       </div>
     </main>
